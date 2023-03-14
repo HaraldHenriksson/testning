458bbe40889715b915477caa8802ca3c4e477592
Harald <Harald@espinagar.se>
2023-03-14 13:26:20 +0100

    added test script, showing verbose result. And test with describe groupe

--- 01-unit-testing/src/sum.test.ts
+++ 01-unit-testing/src/sum.test.ts
@@ -1,17 +1,27 @@
-import { it, expect } from 'vitest'
-import { sum } from "./sum";
+import { describe, it, expect } from 'vitest'
+import { add, sub } from "./sum";
 
-it('should sum two numbers', () => {
+describe('test addition', () => {
+	it('should sum two numbers', () => {
 
-	expect(sum(1, 2)).toBe(3)
+		expect(add(1, 2)).toBe(3)
+	})
+
+	it('should sum three numbers', () => {
+
+		expect(add(1, 2, 3)).toBe(6)
+	})
+
+	it('should sum four numbers', () => {
+
+		expect(add(1, 2, 3, 4)).toBe(10)
+	})
 })
 
-it('should sum three numbers', () => {
-
-	expect(sum(1, 2, 3)).toBe(6)
+describe('test subtraction', () => {
+	it('should subtract 50 from 100', () => {
+		expect(sub(100, 50)).toBe(50)
+	})
 })
 
-it('should sum four numbers', () => {
 
-	expect(sum(1, 2, 3, 4)).toBe(10)
-})
--- 01-unit-testing/src/sum.ts
+++ 01-unit-testing/src/sum.ts
@@ -1,11 +1,16 @@
 
 // Add a and b and return sum
-export const sum = (...numbers: number[]) => {
+export const add = (...numbers: number[]) => {
 
 	const sum = numbers.reduce((acc, num) => {
 		return acc + num
 	}, 0)
 
 	return sum
+}
+
+// subtract numbers from each other and return sum
+export const sub = (initialValue: number, ...numbers: number[]) => {
+	return numbers.reduce((acc, num) => acc - num, initialValue)
 }
 
