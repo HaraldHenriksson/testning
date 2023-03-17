dbbdc475ae1106e28fb6f7d8636cd28345679b67
Harald <Harald@espinagar.se>
2023-03-17 10:50:40 +0100

    render todos in three different functions

--- 02-spying/src/utils/render.ts
+++ 02-spying/src/utils/render.ts
@@ -1,18 +1,33 @@
 import { Todo } from '../types/Todo'
 
+// render output to DOM
+export const render = (elemtnt: HTMLElement, html: string) => {
+	elemtnt.innerHTML = html
+}
+
 // render todos
 export const renderTodos = (todos: Todo[]) => {
+
+	render(
+		document.querySelector<HTMLUListElement>('#todos')!,
+		transformTodosToHtml(todos)
+	)
 	// replace todosList content
 	const todosEl = document.querySelector<HTMLUListElement>('#todos')!
-	todosEl.innerHTML = todos
+	todosEl.innerHTML = transformTodosToHtml(todos)
+}
+
+// transform todos to HTML(-string)
+export const transformTodosToHtml = (todos: Todo[]) => {
+	return todos
 		.map(todo =>
 			`<li class="list-group-item todo ${todo.completed ? 'completed' : ''}" data-todo-id="${todo.id}">
-				<span class="todo-title">${todo.title}</span>
-				${todo.completed
-					? '<span class="delete-todo" role="button" title="Delete todo">🗑️</span>'
-					: ''
-				}
-			</li>`
+			<span class="todo-title">${todo.title}</span>
+			${todo.completed
+				? '<span class="delete-todo" role="button" title="Delete todo">🗑️</span>'
+				: ''
+			}
+		</li>`
 		)
 		.join('')
 }
