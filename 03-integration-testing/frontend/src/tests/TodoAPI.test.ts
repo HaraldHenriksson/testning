61dedfcfee9774100b37015b44a133e200b83c26
Harald <Harald@espinagar.se>
2023-03-21 11:56:51 +0100

    initial test for TodoAPI

---
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest'
+import * as TodoAPI from '../services/TodoAPI'
+
+describe('TodoAPI', () => {
+
+	it.todo('should return a list')
+
+	it.todo('should create a new todo')
+
+	it.todo('should create and then get the todo')
+
+	it.todo('should create and then update a todo')
+
+	it.todo('should create and then delete a todo')
+})
