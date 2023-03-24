137a16f0a463b2b635d42527a13e38feee766958
Harald <Harald@espinagar.se>
2023-03-24 14:20:10 +0100

    e2e tests for entering email

--- 04-cypress/cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ 04-cypress/cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -1,12 +1,36 @@
+import { faker } from '@faker-js/faker'
+
 describe('Newsletter Subscribe', () => {
+	const email = faker.internet.email()
+	const invalidEmail = 'pelle'
+	const subscribedEmail = 'john@example.com'
+
 	beforeEach(() => {
 		cy.visit('http://localhost:3000')
 	})
 
-	it('allows user to subscribe to the email list', () => {
-		//cy.get('[data-test="email-input"]')
-		cy.getByDataTest('email-input').type('pelle@svanslos.nu')
-		cy.getByDataTest('submit-message').should('exist').contains('pelle@svanslos.nu')
-		cy.getByDataTest('success-message').should('exist').contains('pelle@svanslos.nu')
+	it('allows users to subscribe to the email list', () => {
+		// cy.get('[data-test="email-input"]')
+		cy.getByDataTest('email-input').type(email)
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('success-message').should('exist').contains(email)
+	})
+
+	it('displays an error message when the email is invalid', () => {
+		cy.getByDataTest('email-input').type(invalidEmail)
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('success-message').should('not.exist')
+		cy.getByDataTest('server-error-message').should('not.exist')
+	})
+
+	it('should not allow users to subscribe twice', () => {
+		cy.getByDataTest('email-input').type(subscribedEmail)
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('server-error-message').should('exist').contains(subscribedEmail)
+	})
+
+	it('should not allow subscribing without an email adress', () => {
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('error-message').should('exist').contains('Email is required')
 	})
 })
