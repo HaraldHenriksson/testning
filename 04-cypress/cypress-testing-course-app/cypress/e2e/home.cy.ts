b53cfc7bf6d2d2604a52eb8bb8f6fcb92bedea3c
Harald <Harald@espinagar.se>
2023-03-24 15:02:10 +0100

    sections (works with describe also)

--- 04-cypress/cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ 04-cypress/cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -3,27 +3,39 @@ describe('Home Page', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000')
 	})
-	it('the h1 containes the correct text', () => {
 
-		cy.get('[data-test="hero-heading"]')
-			.should('exist')
-			.contains('Testing Next.js Applications with Cypress')
+	context('Hero section', () => {
+		it('the h1 containes the correct text', () => {
+
+			cy.get('[data-test="hero-heading"]')
+				.should('exist')
+				.contains('Testing Next.js Applications with Cypress')
+		})
+
+		it('the fratures on the home page', () => {
+
+			cy.get('dt')
+				.eq(0)
+				.contains('4 Courses')
+
+			cy.get('dt')
+				.eq(1)
+				.contains('25+ Lessons')
+
+			cy.get('dt')
+				.eq(2)
+				.contains('Free and Open Source')
+		})
 	})
 
-	it('the fratures on the home page', () => {
+	context('Courses section', () => {
+		it('can navigate to the first course page', () => {
+			cy.getByDataTest('course-0')
+				.find('a')
+				.eq(-1)
+				.click()
 
-		cy.get('dt')
-			.eq(0)
-			.contains('4 Courses')
-
-		cy.get('dt')
-			.eq(1)
-			.contains('25+ Lessons')
-
-		cy.get('dt')
-			.eq(2)
-			.contains('Free and Open Source')
+			cy.location('pathname').should('equal', '/testing-your-first-application')
+		})
 	})
-
-
 })
