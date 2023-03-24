a46db6f2e9b735908aa6e5f0a556a96f0587b65e
Harald <Harald@espinagar.se>
2023-03-24 11:24:00 +0100

    tests for home page and tsconfig

--- 04-cypress/cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ 04-cypress/cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -1,9 +1,29 @@
 describe('Home Page', () => {
-	it('the h1 containes the correct text', () => {
-		cy.visit('http://localhost:3000')
 
-		cy.get('h1')
+	beforeEach(() => {
+		cy.visit('http://localhost:3000')
+	})
+	it('the h1 containes the correct text', () => {
+
+		cy.get('[data-test="hero-heading"]')
 			.should('exist')
 			.contains('Testing Next.js Applications with Cypress')
 	})
+
+	it('the fratures on the home page', () => {
+
+		cy.get('dt')
+			.eq(0)
+			.contains('4 Courses')
+
+		cy.get('dt')
+			.eq(1)
+			.contains('25+ Lessons')
+
+		cy.get('dt')
+			.eq(2)
+			.contains('Free and Open Source')
+	})
+
+
 })
